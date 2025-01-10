e874c5f5d643176f0d74fa70abdb8207b0559ee4
Woxell <woxellb@gmail.com>
2025-01-10 13:23:50 +0100

    Initial setup done

--- src/API/main.py
+++ src/API/main.py
@@ -1,1 +1,12 @@
-print('Hello world')
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/")
+def hello_world():
+    return "arrrr matey!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
