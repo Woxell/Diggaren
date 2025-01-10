12f80638e05514ee38bcfd05dd56d2c04c6509e4
DeadRed04 <adrian.fagergren@gmail.com>
2025-01-10 14:01:08 +0100

    tooooooooooo easy GG
    yep

--- src/API/sr_api.py
+++ src/API/sr_api.py
@@ -2,17 +2,32 @@ import requests
 url = "https://api.sr.se/api/v2/"
 
 def getSongName(channel):
-    endpoint = f"channels/{channel}/playlists/rightnow"
+    endpoint = f"playlists/rightnow?channelid={channel}"
     params = {"format":"json"}
     response = requests.get(url+endpoint,params=params)
     if response.status_code == 200:
         data = response.json()
-        if 'song' in data and data['song']:
-            song = data['song']
+        if 'playlist' in data and 'song' in data['playlist'] and data['playlist']['song']:
+            song = data['playlist']['song']
             print(f"Title: {song['title']}")
         else:
             print(f"No song playing")
     else:
         print(f"error {response.status_code}: {response.text}")
 
+def getSongArtist(channel):
+    endpoint = f"playlists/rightnow?channelid={channel}"
+    params = {"format":"json"}
+    response = requests.get(url+endpoint,params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if 'playlist' in data and 'song' in data['playlist'] and data['playlist']['song']:
+            song = data['playlist']['song']
+            print(f"Artist: {song['artist']}")
+        else:
+            print(f"No song playing")
+    else:
+        print(f"error {response.status_code}: {response.text}")
+
 getSongName(164)
+getSongArtist(164)
