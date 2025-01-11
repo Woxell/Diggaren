07c8125a58cdf28f824a43397504c1296e07c67d
elleolsson <eliasolsson5@gmail.com>
2025-01-11 10:32:49 +0100

    la ihop allt

--- src/API/test.py
+++ src/API/test.py
@@ -32,35 +32,4 @@ def getSongInfo(channel):
     else:
         print(f"Fel {response.status_code} : {response.text}")
 
-def getSongName(channel):
-    endpoint = f"playlists/rightnow?channelid={channel}"
-    params = {"format": "json"}
-    response = requests.get(url + endpoint, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        if 'playlist' in data and 'song' in data['playlist'] and data['playlist']['song']:
-            song = data['playlist']['song']
-            print(f"Title: {song['title']}")
-        else:
-            print(f"No song playing")
-    else:
-        print(f"error {response.status_code}: {response.text}")
-
-
-def getSongArtist(channel):
-    endpoint = f"playlists/rightnow?channelid={channel}"
-    params = {"format": "json"}
-    response = requests.get(url + endpoint, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        if 'playlist' in data and 'song' in data['playlist'] and data['playlist']['song']:
-            song = data['playlist']['song']
-            print(f"Artist: {song['artist']}")
-        else:
-            print(f"No song playing")
-    else:
-        print(f"error {response.status_code}: {response.text}")
-
-getSongName(164)
-getSongArtist(164)
 getSongInfo(164)
