f39f2c0253438323f07c826b8cb602dd5d44d6a0
DeadRed04 <adrian.fagergren@gmail.com>
2025-01-11 14:19:06 +0100

    test gone!

--- src/API/sr_api.py
+++ src/API/sr_api.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime
 
 url = "https://api.sr.se/api/v2/"
-testChanel = 164
+testChannel = 164
 def getChannel(channel):
     endpoint = f"playlists/rightnow?channelid={channel}"
     params = {"format": "json"}
@@ -67,8 +67,43 @@ def getStartTime(channel):
     else:
         return f"error {response.status_code}: {response.text}"
 
+def getLastSong(channel):
+    endpoint = f"playlists/rightnow?channelid={channel}"
+    params = {"format": "json"}
+    response = requests.get(url + endpoint, params=params)
+    if response.status_code == 200:
+        data = response.json()
 
-print(getChannel(testChanel))
-print(getSongName(testChanel))
-print(getSongArtist(testChanel))
-print(getStartTime(testChanel))
+        if 'playlist' in data and 'previoussong' in data['playlist']:
+            song = data['playlist']['previoussong']
+            channelName = data.get('playlist', {}).get('channel', {}).get('name', 'okänd kanal')
+            title = song.get('title', 'okänd titel')
+            artist = song.get('artist', 'okänd artist')
+            startTime = song.get('starttimeutc', None)
+            stopTime = song.get('stoptimeutc', None)
+            if startTime:
+                timestamp_ms = int(startTime.strip("/Date()"))
+                startTime = datetime.fromtimestamp(timestamp_ms / 1000)  # Konvertera till sekunder
+                startTime = startTime.strftime("%Y-%m-%d %H:%M:%S")
+
+                timestamp_ms2 = int(stopTime.strip("/Date()"))
+                stopTime = datetime.fromtimestamp(timestamp_ms2 / 1000)  # Konvertera till sekunder
+                stopTime = stopTime.strftime("%Y-%m-%d %H:%M:%S")
+
+            else:
+                startTime = "Ingen starttid kompis"
+                stopTime = "Ingen stopptid kombis"
+            return f"Kanal: {channelName}\n Artist: {artist}\n Låt: {title}\n Starttid: {startTime}\n Stopptid: {stopTime}"
+        else:
+            return "Ingen låt spelas nu kingen"
+    else:
+        return f"Fel {response.status_code} : {response.text}"
+
+
+
+print(getChannel(testChannel))
+print(getSongName(testChannel))
+print(getSongArtist(testChannel))
+print(getStartTime(testChannel))
+print(getLastSong(testChannel))
+
