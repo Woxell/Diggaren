4408a5b846cb2b223f9735a1985615c89bd281f4
elleolsson <eliasolsson5@gmail.com>
2025-01-11 10:33:38 +0100

    Det samlade i en testfil

--- src/API/sr_api.py
+++ src/API/sr_api.py
@@ -1,36 +1,6 @@
 import requests
-from datetime import datetime
 
 url = "https://api.sr.se/api/v2/"
-
-
-def getSongInfo(channel):
-    endpoint = f"playlists/rightnow?channelid={channel}"
-    params = {"format": "json"}
-    response = requests.get(url + endpoint, params=params)
-    if response.status_code == 200:
-        data = response.json()
-
-        if 'playlist' in data and 'song' in data['playlist'] and data['playlist']['song']:
-            song = data['playlist']['song']
-            channelName = data.get('channel', {}).get('name', 'okänd kanal')
-            title = song.get('title', 'okänd titel')
-            artist = song.get('artist', 'okänd artist')
-            startTime = song.get('starttimeutc', None)
-
-            if startTime:
-                startTime = datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%SZ")
-                startTime = datetime.strftime("%Y-%m-%d %H:%M:%S")
-            else:
-                startTime = "Ingen starttid kompis"
-            print(f"Kanal: {channelName}")
-            print(f"Artist: {artist}")
-            print(f"Låt: {title}")
-            print(f"Klockslag: {startTime}")
-        else:
-            print("Ingen låt spelas nu kingen")
-    else:
-        print(f"Fel {response.status_code} : {response.text}")
 
 def getSongName(channel):
     endpoint = f"playlists/rightnow?channelid={channel}"
@@ -63,4 +33,3 @@ def getSongArtist(channel):
 
 getSongName(164)
 getSongArtist(164)
-getSongInfo(164)
